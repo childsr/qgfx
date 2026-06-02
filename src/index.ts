376b2e7f456599e3c8d36f03b01405160ae45866
Ryan Childs <rchilds24@gmail.com>
2026-06-02 12:39:37 -0600

    feat: enhance Picture API with new shape functions and shadow properties

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,51 @@
-import {
+import type {
   Picture,
   Transform,
   FillStyle,
   LineStyle,
   ImageSource,
   ImageSourceData,
-  ShadowOptions
+  ShadowOptions,
+  Color,
+  Matrix,
+  MatrixArray
 } from "./types"
 import { Path } from "./path"
 import { isColor } from "./color"
 import { Gradient } from "./gradient"
+import { Pattern } from "./pattern"
 
-export const blank: Picture = _ctx => {}
+export const blank: Picture = _ctx => { }
 export const rect: Picture = ctx => {
-  ctx.rect(0,0,1,1)
+  ctx.rect(0, 0, 1, 1)
 }
 export const circle: Picture = ctx => {
-  ctx.moveTo(0,1)
-  ctx.arc(0,0,1,0,Math.PI*2)
+  ctx.moveTo(0, 1)
+  ctx.arc(0, 0, 1, 0, Math.PI * 2)
 }
 export const segment = (x1: number, y1: number, x2: number, y2: number): Picture => {
   return ctx => {
     ctx.moveTo(x1, y1)
     ctx.lineTo(x2, y2)
+  }
+}
+export const arc = (radius: number, start: number, end: number): Picture => {
+  return ctx => {
+    ctx.arc(0, 0, radius, start, end)
+  }
+}
+export const quadraticCurve = (
+
+  cpx: number, cpy: number,
+  x: number,   y: number
+): Picture => {
+  return ctx => {
+    ctx.quadraticCurveTo(cpx, cpy, x, y)
+  }
+}
+export const bezierCurve = (cp1x: number, cp1y: number, cp2x: number, cp2y: number, x: number, y: number): Picture => {
+  return ctx => {
+    ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y)
   }
 }
 export const fillText = (content: string, font?: string): Picture => {
@@ -39,19 +62,16 @@ export const strokeText = (content: string, font?: string): Picture => {
   }
 }
 
-export const arc = (radius: number, start: number, end: number): Picture => {
-  return ctx => {
-    ctx.arc(0, 0, radius, start, end)
-  }
-}
-
 export const clear: Picture = ctx => {
   ctx.canvas.width += 0
 }
 
 export const iterate = (n: number, fn: (i: number) => Picture): Picture => {
-  const pics = Array(n).fill(null).map((_,i) => fn(i))
+  const pics = Array(n).fill(null).map((_, i) => fn(i))
   return overlay(pics)
+}
+export const withTransforms = (transforms: Transform[]): Transform => {
+  return pic => overlay(transforms.map(tfm => tfm(pic)))
 }
 
 export const withContext = (pic: Picture): Picture => {
@@ -66,7 +86,7 @@ export const compose = (...transforms: Transform[]): Transform => {
   return pic => transforms.reduceRight((p, t) => t(p), pic)
 }
 
-export const transform = (matrix: DOMMatrix): Transform => {
+const transformDomMtx = (matrix: DOMMatrix): Transform => {
   return pic => {
     return ctx => {
       ctx.transform(matrix.a, matrix.b, matrix.c, matrix.d, matrix.e, matrix.f)
@@ -74,10 +94,22 @@ export const transform = (matrix: DOMMatrix): Transform => {
     }
   }
 }
+const transformMtxArr = (matrix: MatrixArray): Transform => {
+  return pic => {
+    return ctx => {
+      ctx.transform(matrix[0], matrix[1], matrix[2], matrix[3], matrix[4], matrix[5])
+      pic(ctx)
+    }
+  }
+}
+export const transform = (matrix: Matrix): Transform => {
+  if (matrix instanceof DOMMatrix) return transformDomMtx(matrix)
+  else return transformMtxArr(matrix)
+}
 export const translate = (x: number, y: number): Transform => {
   return pic => {
     return ctx => {
-      ctx.translate(x,y)
+      ctx.translate(x, y)
       pic(ctx)
     }
   }
@@ -85,7 +117,7 @@ export const translate = (x: number, y: number): Transform => {
 export const scale = (x: number, y: number): Transform => {
   return pic => {
     return ctx => {
-      ctx.scale(x,y)
+      ctx.scale(x, y)
       pic(ctx)
     }
   }
@@ -108,27 +140,27 @@ export const skew = (x: number, y: number): Transform => {
 }
 export const flipX: Transform = pic => {
   return ctx => {
-    ctx.scale(-1,1)
+    ctx.scale(-1, 1)
     pic(ctx)
   }
 }
 export const flipY: Transform = pic => {
   return ctx => {
-    ctx.scale(1,-1)
+    ctx.scale(1, -1)
     pic(ctx)
   }
 }
 export const mirrorX: Transform = pic => {
   return ctx => {
     pic(ctx)
-    ctx.scale(-1,1)
+    ctx.scale(-1, 1)
     pic(ctx)
   }
 }
 export const mirrorY: Transform = pic => {
   return ctx => {
     pic(ctx)
-    ctx.scale(1,-1)
+    ctx.scale(1, -1)
     pic(ctx)
   }
 }
@@ -143,18 +175,58 @@ export const opacity = (alpha: number): Transform => {
     }
   }
 }
+export const hueRotate = (radians: number): Transform => {
+  return filter(`hue-rotate(${radians}rad)`)
+}
+export const saturate = (percentage: number): Transform => {
+  return filter(`saturate(${percentage}%)`)
+}
+export const lighten = (percentage: number): Transform => {
+  return filter(`brightness(${percentage}%)`)
+}
 
 export const composite = (operation: GlobalCompositeOperation): Transform => {
   return pic => {
     return ctx => {
-      const oldOperation = ctx.globalCompositeOperation
+      const oldValue = ctx.globalCompositeOperation
       ctx.globalCompositeOperation = operation
       pic(ctx)
-      ctx.globalCompositeOperation = oldOperation
+      ctx.globalCompositeOperation = oldValue
     }
   }
 }
 
+export const shadowColor = (color: string | Color): Transform => {
+  return pic => {
+    return ctx => {
+      ctx.save()
+      ctx.shadowColor = color.toString()
+      pic(ctx)
+      ctx.restore()
+    }
+  }
+}
+export const shadowBlur = (blur: number): Transform => {
+  return pic => {
+    return ctx => {
+      ctx.save()
+      ctx.shadowBlur = blur
+      pic(ctx)
+      ctx.restore()
+    }
+  }
+}
+export const shadowOffset = (x: number, y: number): Transform => {
+  return pic => {
+    return ctx => {
+      ctx.save()
+      ctx.shadowOffsetX = x
+      ctx.shadowOffsetY = y
+      pic(ctx)
+      ctx.restore()
+    }
+  }
+}
 export const shadow = (options: ShadowOptions): Transform => {
   return pic => {
     return ctx => {
@@ -168,6 +240,7 @@ export const shadow = (options: ShadowOptions): Transform => {
     }
   }
 }
+
 export const filter = (filterStr: string): Transform => {
   return pic => {
     return ctx => {
@@ -177,7 +250,7 @@ export const filter = (filterStr: string): Transform => {
   }
 }
 
-export const overlay = (pics: Picture[]) : Picture => {
+export const overlay = (pics: Picture[]): Picture => {
   return ctx => {
     for (let i = 0; i < pics.length; i++) {
       ctx.save()
@@ -191,7 +264,7 @@ export const fill = (fillStyle: FillStyle): Transform => {
   return pic => {
     return ctx => {
       ctx.beginPath()
-      ctx.fillStyle = toCanvasFillStyle(ctx,fillStyle)
+      ctx.fillStyle = toCanvasFillStyle(ctx, fillStyle)
       pic(ctx)
       ctx.fill()
     }
@@ -204,8 +277,8 @@ export const stroke = (strokeStyle: FillStyle, lineStyle?: LineStyle): Transform
       ctx.save()
       pic(ctx)
       ctx.restore()
-      ctx.strokeStyle = toCanvasFillStyle(ctx,strokeStyle)
-      if (lineStyle) applyLineStyle(ctx,lineStyle)
+      ctx.strokeStyle = toCanvasFillStyle(ctx, strokeStyle)
+      if (lineStyle) applyLineStyle(ctx, lineStyle)
       ctx.stroke()
     }
   }
@@ -215,7 +288,7 @@ export const fillStyle = (fillStyle: FillStyle): Transform => {
   return pic => {
     return ctx => {
       const old = ctx.fillStyle
-      ctx.fillStyle = toCanvasFillStyle(ctx,fillStyle)
+      ctx.fillStyle = toCanvasFillStyle(ctx, fillStyle)
       pic(ctx)
       ctx.fillStyle = old
     }
@@ -225,7 +298,7 @@ export const strokeStyle = (strokeStyle: FillStyle): Transform => {
   return pic => {
     return ctx => {
       const old = ctx.strokeStyle
-      ctx.strokeStyle = toCanvasFillStyle(ctx,strokeStyle)
+      ctx.strokeStyle = toCanvasFillStyle(ctx, strokeStyle)
       pic(ctx)
       ctx.strokeStyle = old
     }
@@ -234,7 +307,7 @@ export const strokeStyle = (strokeStyle: FillStyle): Transform => {
 export const lineStyle = (style: LineStyle): Transform => {
   return pic => {
     return ctx => {
-      applyLineStyle(ctx,style)
+      applyLineStyle(ctx, style)
       pic(ctx)
     }
   }
@@ -243,15 +316,15 @@ export const lineStyle = (style: LineStyle): Transform => {
 export const fillAll = (fillStyle: FillStyle): Picture => {
   return ctx => {
     ctx.save()
-    ctx.setTransform(1,0,0,1,0,0)
-    ctx.fillStyle = toCanvasFillStyle(ctx,fillStyle)
+    ctx.setTransform(1, 0, 0, 1, 0, 0)
+    ctx.fillStyle = toCanvasFillStyle(ctx, fillStyle)
     ctx.beginPath()
-    ctx.fillRect(0,0,ctx.canvas.width,ctx.canvas.height)
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
     ctx.restore()
   }
 }
 export const image = (imageSource: ImageSource, data?: ImageSourceData): Picture => {
-  if (!data) return ctx => ctx.drawImage(imageSource,0,0)
+  if (!data) return ctx => ctx.drawImage(imageSource, 0, 0)
   else return ctx => ctx.drawImage(
     imageSource,
     data.sx,
@@ -277,9 +350,9 @@ export const clip = (path: Path): Transform => {
   }
 }
 
-export const path = new Path()
+export const path = Path.path
 export const addPaths = (path1: Path, path2: Path, ...rest: Path[]) => {
-  return Path.from([path1,path2,...rest])
+  return Path.from([path1, path2, ...rest])
 }
 
 /* Utilities */
@@ -290,23 +363,25 @@ export const radiansToDegrees = (radians: number) => radians * 180 / Math.PI
 /* Aliases */
 
 export const move = translate
-export const moveX = (x: number) => move(x,0)
-export const moveY = (y: number) => move(0,y)
-export const sc = (s: number) => scale(s,s)
-export const scaleX = (x: number) => scale(x,1)
-export const scaleY = (y: number) => scale(1,y)
+export const moveX = (x: number) => move(x, 0)
+export const moveY = (y: number) => move(0, y)
+export const sc = (s: number) => scale(s, s)
+export const scaleX = (x: number) => scale(x, 1)
+export const scaleY = (y: number) => scale(1, y)
 export const rot = rotate
 export const rotateDeg = (degrees: number) => rotate(degreesToRadians(degrees))
 export const rotDeg = rotateDeg
-export const skewX = (x: number) => skew(x,0)
-export const skewY = (y: number) => skew(0,y)
+export const skewX = (x: number) => skew(x, 0)
+export const skewY = (y: number) => skew(0, y)
 export const deg2Rad = degreesToRadians
 export const rad2Deg = radiansToDegrees
 
 
+export * from "./color"
 export * from "./gradient"
 export * from "./image"
-export * from "./color"
+export * from "./path"
+export * from "./pattern"
 export type * from "./types"
 
 function applyLineStyle(ctx: CanvasRenderingContext2D, lineStyle: LineStyle) {
@@ -320,5 +395,6 @@ function applyLineStyle(ctx: CanvasRenderingContext2D, lineStyle: LineStyle) {
 function toCanvasFillStyle(ctx: CanvasRenderingContext2D, s: FillStyle): CanvasFillStrokeStyles["fillStyle"] {
   if (isColor(s)) return s.toString()
   else if (s instanceof Gradient) return s.getCanvasGradient(ctx)
+  else if (s instanceof Pattern) return s.getCanvasPattern(ctx)
   else return s
 }
